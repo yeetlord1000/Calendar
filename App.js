9f73891e0db0f84d528117cd67e46181c560c49e
yeetlord1000 <76589036+yeetlord1000@users.noreply.github.com>
2021-05-19 20:55:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import BasicCalendar from './components/BasicCalendar'
+
+function App() {
+  return (
+    <BasicCalendar />
+  );
+}
+
+export default App;
---
+++ components/BasicCalendar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Calendar, Views, momentLocalizer } from 'react-big-calendar'
+import events from '../utils/events'
+import * as dates from '../utils/dates'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+const localizer = momentLocalizer(moment)
+
+let allViews = Object.keys(Views).map(k => Views[k])
+
+const ColoredDateCellWrapper = ({ children }) =>
+  React.cloneElement(React.Children.only(children), {
+    style: {
+      backgroundColor: 'lightblue',
+    },
+  })
+
+let BasicCalendar = () => (
+  <Calendar
+    events={events}
+    views={allViews}
+    step={60}
+    showMultiDayTimes
+    max={dates.add(dates.endOf(new Date(2015, 17, 1), 'day'), -1, 'hours')}
+    defaultDate={new Date(2015, 3, 1)}
+    components={{
+      timeSlotWrapper: ColoredDateCellWrapper,
+    }}
+    localizer={localizer}
+  />
+)
+
+export default BasicCalendar
